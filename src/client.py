81ebcb574beaf540ff252f5b360cdc0dd3ad48ca
carcasonne <villumsonne@tutanota.com>
2022-10-21 16:10:50 +0200

    Clients can now tlak to each other without going insane

--- src/client.py
+++ src/client.py
@@ -1,3 +1,5 @@
+import atexit
+import time
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from random import randint
@@ -11,36 +13,74 @@ class Client(DatagramProtocol):
 
         self.Id = host, port
         self.Server = "127.0.0.1", ServerPort
-        self.Address = None
+        self.Connection = None
+        self.ConnectionName = None
         self.Username = None
 
         print("Client has been created with ID:", self.Id)
     
     def startProtocol(self):
-        self.transport.write("ready".encode('utf-8'), self.Server)
+        self.transport.write(("Requesting possible connections*" + self.Username).encode('utf-8'), self.Server)
 
     def datagramReceived(self, datagram: bytes, addr):
         datagram = datagram.decode('utf-8')
         
-        # TODO: base this around username instead
+        # This is how the client connects to other clients (by getting a list from a central server)
+        # Note: This assignment is not about making a pretty, efficient, or sane client-server connection... :) 
         if addr == self.Server:
             print("Choose a client from these: \n", datagram)
-            self.Address = input("Write host:"), int(input("Write port:"))
-            reactor.callInThread(self.sendMessage)
-        else:
-            print(addr, ":", datagram )
-        
-    def sendMessage(self):
-        try:
-            while True:
-                self.transport.write(input(":::").encode('utf-8'), self.Address)
-        finally:
-            print("hej")
-            # Some code to inform server that this client has lost connection
 
+            if(datagram == ""):
+                print("No possible connections exists")
+                print("Attempting to find connections again in 5 seconds...")
+                time.sleep(5)
+                self.startProtocol()
+            else:
+                # All of this would be much prettier if I just sent structs instead of raw strings
+                split = datagram.split("\n")
+                split.pop(0)
+                connection = -1
+
+                while True:
+                    connection = input("Write ID of connection to use: ")
+                    if 0 <= int(connection) < len(split):
+                        break
+                
+                splitMessage = split[int(connection)].split(',')
+                port = splitMessage[1]
+                name = splitMessage[2]
+                port = self.trim(port)
+                name = self.trim(name)
+
+                self.Connection = "127.0.0.1", int(port)
+                self.ConnectionName = name
+
+                print("Connected to: " + name)
+
+                # infinte loop in a thread to keep checking for user input
+                reactor.callInThread(self.sendMessage)
+        else:
+            print("(" + self.ConnectionName + "): ", datagram )
+
+    def trim(self, name):
+        name = name.strip()
+        name = name.strip(')')
+        name = name.strip("'")
+        return name;
     
+    def sendMessage(self):
+        while True:
+            msg = input()
+            print("\033[A                             \033[A") #hack to remove the users own input from terminal
+                                                                #done just to make it prettier
+            print("(You): " + msg)
+            self.transport.write(msg.encode('utf-8'), self.Connection)
+
 if __name__ == "__main__":
     Port = randint(4000, 5000)
     Client = Client("localhost", Port)
+
+    Client.Username = input("Choose username:")
+
     reactor.listenUDP(Port, Client)
     reactor.run()
--- src/server.py
+++ src/server.py
@@ -8,12 +8,34 @@ class Server(DatagramProtocol):
     
     def datagramReceived(self, datagram: bytes, addr):
         datagram = datagram.decode('utf-8')
-        if datagram == "ready":
-            addresses = "\n".join([str(x) for x in self.clients])
+
+        split = datagram.split('*')
+        request = split[0]
+        name    = ""
+
+        if(len(split) > 1):
+            name = split[1]
+
+        if request == "Requesting possible connections":
+            allOtherClients = self.clients
+            if (addr, name) in allOtherClients:
+                allOtherClients.remove((addr, name))
+
+            addresses = ""
+
+            counter = 0;
+
+            for client in self.clients:
+                addresses += "\n"
+                addresses += "Connection with ID " + str(counter) + ": "
+                addresses += str(client)
+                counter   += 1
+
+            print(name + " has requested access to all known connections")
 
             # TODO: also include username here
             self.transport.write(addresses.encode('utf-8'), addr)
-            self.clients.add(addr)
+            self.clients.add((addr, name))
 
 if __name__ == "__main__":
     reactor.listenUDP(9999, Server())
